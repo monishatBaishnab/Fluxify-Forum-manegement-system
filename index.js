d198b719f491de08866404255314808964379999
monishat <monishat@gmail.com>
2023-11-24 19:27:56 +0600

    Apply all nessesary middlewores.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const globalErrorHandler = require('./src/utils/globalErrorHandler');
 const pathErrorHandler = require('./src/utils/pathErrorHandler');
+const applyMiddlewores = require('./src/middlewares');
 const app = express();
 const port = process.env.PORT || 5000;
+
+applyMiddlewores(app);
 
 app.get('/health', (req, res) => {
     res.send('Flucify server running..');
